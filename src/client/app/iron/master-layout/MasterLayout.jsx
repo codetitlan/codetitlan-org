93df9ae190819f3d7140b5ed1a1ac787d6d58d68
Eduardo M(bbaaxx) <bbaaxx@gmail.com>
2017-09-04 17:08:38 -0500

    integrated rsm-button

--- src/client/app/iron/master-layout/MasterLayout.jsx
+++ src/client/app/iron/master-layout/MasterLayout.jsx
@@ -4,14 +4,10 @@ import xs from 'xstream';
 import { html } from 'snabbdom-jsx';
 import RsmButton from '../../wood/rsm-button';
 
-function intent(sources) {
-  const rsmButton = RsmButton({ DOM: sources.DOM, props: xs.of({ text: 'Click me dude' }) });
-  const rsmButtonVdom$ = rsmButton.DOM;
-  const rsmButtonClick$ = rsmButton.click$;
-
+function intent(sources, scrollButtonClick$) {
   return {
     scrollUpdate$: sources.Scroll.startWith(0),
-    newClick$: sources.DOM.select('.scroll-down-button').events('click'),
+    newClick$: scrollButtonClick$,
   };
 }
 
@@ -21,23 +17,33 @@ function model(actions) {
   return {
     scrollPosition$,
     scrollDownClick$: click$.mapTo(500),
-    log$: click$.map(val => val.target),
+    log$: click$.map(clickEvt => clickEvt),
   };
 }
 
-function view(state$) {
-  return state$.scrollPosition$.map(value => (
-    <div className="scroll-display">
-      <span>{value}</span>
-      <button className="scroll-down-button">Scroll down</button>
-    </div>
-  ));
+function view(state$, scrollButtonVdom$) {
+  return xs.combine(state$.scrollPosition$, scrollButtonVdom$)
+    .map(([scrollPosition, scrollButtonVdom]) => (
+      <div className="scroll-display">
+        <span>{scrollPosition}</span>
+        {scrollButtonVdom}
+      </div>
+    ));
 }
 
 export default function MasterLayout(sources) {
-  const state$ = model(intent(sources));
+  const rsmButton = RsmButton({
+    DOM: sources.DOM,
+    props: xs.of({ text: 'Click me dude', className: 'scroll-down-button' }),
+  });
+  const scrollButtonClick$ = rsmButton.click$;
+  const scrollButtonVdom$ = rsmButton.DOM;
+
+  const actions = intent(sources, scrollButtonClick$);
+  const state$ = model(actions);
+
   return {
-    DOM: view(state$),
+    DOM: view(state$, scrollButtonVdom$),
     Scroll: state$.scrollDownClick$,
     log: state$.log$,
   };
