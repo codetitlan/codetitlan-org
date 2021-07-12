eccca3d9012639829536fb0d2cef330bc919f1d8
Ed Mosqueda (bbaaxx) <bbaaxx@gmail.com>
2021-07-12 00:25:50 -0500

    simple tw test

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Typewriter from 'typewriter-effect';
+import typewriter from 'typewriter-effect';
+
+const welcomeMessage = [
+  'The answer to life' +
+    '\n' +
+    'the universe and everything!' +
+    '\n' +
+    'is 42!',
+];
 
 export function HomePage() {
   return (
@@ -8,7 +18,30 @@ export function HomePage() {
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <span>HomePage container</span>
+      <Typewriter
+        options={{
+          strings: welcomeMessage,
+          autoStart: true,
+          loop: true,
+        }}
+        onInit={tw => tw.start()}
+      />
+
+      {/* <Typewriter
+        onInit={typewriter => {
+          typewriter
+            .typeString('is 42!')
+            .callFunction(() => {
+              console.log('String typed out!');
+            })
+            .pauseFor(2500)
+            .deleteAll()
+            .callFunction(() => {
+              console.log('All strings were deleted');
+            })
+            .start();
+        }}
+      /> */}
     </>
   );
 }
