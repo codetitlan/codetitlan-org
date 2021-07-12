cc74c58d049d00c47aebfa7d9bf88c95d685ed85
Ed Mosqueda (bbaaxx) <bbaaxx@gmail.com>
2021-07-11 21:09:56 -0500

    added simple typewritter effect test

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Typewriter from 'typewriter-effect';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString('Hello World!')
+            .callFunction(() => {
+              console.log('String typed out!');
+            })
+            .pauseFor(2500)
+            .deleteAll()
+            .callFunction(() => {
+              console.log('All strings were deleted');
+            })
+            .start();
+        }}
+      />
+    </>
   );
 }
 
